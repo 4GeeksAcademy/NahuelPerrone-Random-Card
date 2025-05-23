8819805fd0033a9a4703b923ef770d362e6a18bf
NahuelPerrone85 <nahuelperrone85@gmail.com>
2025-05-23 22:20:56 +0000

    primera etapa

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+let randomCard = () => {
+// let classCard =["Corazones", "Picas", "Tréboles" ,"Diamantes"];
+// let randomClassCard = classCard [Math.floor(Math.random()*4)];
+let number = [2,3,4,5,6,7,8,9,10,"Rey","Reina","jota","Az"];
+let randomNumber = number [Math.floor(Math.random()*13)];
+
+// let randomFinal = `${randomClassCard} ${randomNumber}`;
+// return randomFinal
+return randomNumber
+}
+
+document.getElementById("aleatorio").innerHTML = randomCard ()
+
+let hola = () => {
+
+let classCard =["Corazones", "Picas", "Tréboles" ,"Diamantes"];
+let randomClassCard = classCard [Math.floor(Math.random()*4)];
+
+return randomClassCard
+}
+
+document.getElementById("card-palo").innerHTML = hola ()
+
 };
